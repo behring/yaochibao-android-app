b518baea019d26a4c46dc947c6ad7ea59ff625ce
behring <behring_zhao@126.com>
2021-04-15 23:28:27 +0800

    重构getfoods api

---
+++ mobile-bff/controllers/foods.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getFoods: (req, res) => {
+        res.json(mockFoods())
+    }
+}
+
+const mockFoods = () => {
+    var foods = []
+    for (var index = 0; index < 20; index++) {
+        var food = { name: `鸡排饭${index}`, priceCent: 3200, imageUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2481205946,34418544&fm=26&gp=0.jpg" }
+        foods.push(food)
+    }
+    return foods
+}
--- mobile-bff/index.js
+++ mobile-bff/index.js
@@ -1,15 +1,9 @@
 const express = require('express')
+const foods = require("./controllers/foods")
 const app = express()
 const port = 3000
 
-app.get('/customer/foods', (req, res) => {
-  var foods = []
-  for (var index = 0; index < 20; index++) {
-    var food={name:`鸡排饭${index}`,priceCent:3200, imageUrl:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2481205946,34418544&fm=26&gp=0.jpg"}
-    foods.push(food)
-  }
-  res.json(foods)
-})
+app.get('/customer/foods', foods.getFoods)
 
 app.listen(port, () => {
   console.log(`Mobile BFF listening at http://localhost:${port}`)
