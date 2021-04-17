498e0e206f911173340a80136a7729d14f3f4c55
behring <behring_zhao@126.com>
2021-04-17 13:32:06 +0800

    重构FoodsPresenterTest

--- YaoChiBaoApp/app/src/test/java/behring/android/yaochibao/domain/FoodsPresenterTest.java
+++ YaoChiBaoApp/app/src/test/java/behring/android/yaochibao/domain/FoodsPresenterTest.java
@@ -2,7 +2,10 @@ package behring.android.yaochibao.domain;
 
 import android.content.Context;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,17 +26,25 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class FoodsPresenterTest {
+    @Mock
+    FoodsRepository mockFoodsRepository;
+    FoodsPresenter foodsPresenter;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        foodsPresenter = spy(new FoodsPresenter(mock(Context.class), mockFoodsRepository));
+    }
+
     @Test
     public void should_return_foods_to_view_model_when_call_presenter_get_foods_and_network_available() {
         //given
-        FoodsRepository stubFoodsRepository = mock(FoodsRepository.class);
-        when(stubFoodsRepository.getFoods(anyString(), anyInt(), anyInt()))
+        when(mockFoodsRepository.getFoods(anyString(), anyInt(), anyInt()))
                 .thenReturn(Single.just(Arrays.asList(mock(Food.class), mock(Food.class))));
-        FoodsPresenter spyPresenter = spy(new FoodsPresenter(mock(Context.class), stubFoodsRepository));
-        doReturn(true).when(spyPresenter).isNetworkAvailable();
-        FoodsViewModel viewModel = new FoodsViewModel(spyPresenter);
+        doReturn(true).when(foodsPresenter).isNetworkAvailable();
+        FoodsViewModel viewModel = new FoodsViewModel(foodsPresenter);
         //when
-        List<Food> presentFoods = spyPresenter.getFoods(anyString(), anyInt(), anyInt()).blockingGet();
+        List<Food> presentFoods = foodsPresenter.getFoods(anyString(), anyInt(), anyInt()).blockingGet();
         //then
         viewModel.loadFoods((foods, throwable) -> assertEquals(foods, presentFoods));
     }
@@ -41,12 +52,10 @@ public class FoodsPresenterTest {
     @Test
     public void should_call_repository_get_foods_when_call_presenter_get_foods_and_network_available() {
         //given
-        FoodsRepository mockFoodsRepository = mock(FoodsRepository.class);
         when(mockFoodsRepository.getFoods(anyString(), anyInt(), anyInt())).thenReturn(Single.just(new ArrayList<>()));
-        FoodsPresenter spyPresenter = spy(new FoodsPresenter(mock(Context.class), mockFoodsRepository));
-        doReturn(true).when(spyPresenter).isNetworkAvailable();
+        doReturn(true).when(foodsPresenter).isNetworkAvailable();
         //when
-        spyPresenter.getFoods(anyString(), anyInt(), anyInt()).blockingSubscribe();
+        foodsPresenter.getFoods(anyString(), anyInt(), anyInt()).blockingSubscribe();
         //then
         verify(mockFoodsRepository).getFoods(anyString(), anyInt(), anyInt());
     }
