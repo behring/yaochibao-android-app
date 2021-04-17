8c0a1b8e11eeea8da2fdbf4845caecab654942f6
behring <behring_zhao@126.com>
2021-04-17 11:44:48 +0800

    修复food没有返回id的问题。到时候mobile数据库DB没有主键导致crash

--- mobile-bff/controllers/foods.js
+++ mobile-bff/controllers/foods.js
@@ -7,7 +7,7 @@ module.exports = {
 const mockFoods = () => {
     var foods = []
     for (var index = 0; index < 20; index++) {
-        var food = { name: `鸡排饭${index}`, priceCent: 3200, imageUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2481205946,34418544&fm=26&gp=0.jpg" }
+        var food = { id: index.toString(), name: `鸡排饭${index}`, priceCent: 3200, imageUrl: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2481205946,34418544&fm=26&gp=0.jpg" }
         foods.push(food)
     }
     return foods
